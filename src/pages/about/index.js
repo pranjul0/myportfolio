b172b112381f861ccb4d6d4d4960fc3703241ca9
Pranjul Parnami <82120286+pranjul0@users.noreply.github.com>
2025-02-12 11:21:26 +0530

    Update index.js

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -35,26 +35,6 @@ export const About = () => {
             </div>
           </Col>
         </Row>
-        {/* <Row className=" sec_sp">
-          <Col lg="5">
-            <h3 className="color_sec py-4">Work Timline</h3>
-          </Col>
-          <Col lg="7">
-            <table className="table caption-top">
-              <tbody>
-                {worktimeline.map((data, i) => {
-                  return (
-                    <tr key={i}>
-                      <th scope="row">{data.jobtitle}</th>
-                      <td>{data.where}</td>
-                      <td>{data.date}</td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </Col>
-        </Row> */}
         <Row className="sec_sp">
           <Col lg="5">
             <h3 className="color_sec py-4">Skills</h3>
